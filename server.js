a4901758bd9b867b448e49167bd79b0b62dc6ef7
klhelker <57188023+klhelker@users.noreply.github.com>
2020-03-02 21:15:44 -0600

    updates

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -40,6 +40,10 @@ var characters = [
 
 // Routes
 // =============================================================
+
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
@@ -86,6 +90,7 @@ app.post("/api/characters", function(req, res) {
 
   res.json(newCharacter);
 });
+
 
 // Starts the server to begin listening
 // =============================================================
